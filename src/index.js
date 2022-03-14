43d47e6c917a35da987ad093e49dbd40d26df256
Abanoub Zaky <a.zaky@octavianlab.com>
2022-03-14 17:58:29 +0100

    add preload

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import Phaser from "phaser";
 
+//Loading Assets, such as images, music, animations ...
+function preload() {
+  this.load.image('sky', 'assets/sky.png')
+}
+
+// create objects instance 
+function create(){
+  
+}
+
 const config = {
   //WebGL ( Web Grachics Library) JS API for rendering 2D and 3D graphics
   type: Phaser.AUTO,
-  width: "100%",
-  height: "100%",
+  width: 800,
+  height: 600,
   physics: {
     //Arcade physics plugin
     default: "arcade",
   },
+  scene: {
+    preload,
+    create,
+  }
 };
+
+
 
 new Phaser.Game(config);
